93a35f01dcda8f791782fd3943e980cb09bf6594
Sergei-SLS <lyamin.sls@gmail.com>
2025-05-17 22:11:15 +0500

    refactor: homeWorkReducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,13 +4,13 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             return [...state].sort((a, b) => action.payload === 'up'
             ? a.name.localeCompare(b.name)
             : b.name.localeCompare(a.name)
-        )
+        )}
         case 'check': {
             return state.filter(user => user.age >= action.payload) // need to fix
         }
