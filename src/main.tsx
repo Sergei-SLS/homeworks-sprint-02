26844b020df4a592c00087db3b94cea018d5fa34
Sergei-SLS <lyamin.sls@gmail.com>
2025-05-31 15:10:24 +0500

    style: add style loader

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from '../src/s1-main/App.tsx'
+import {Provider} from "react-redux";
+import store from "./s2-homeworks/hw10/bll/store.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
   </StrictMode>,
 )
