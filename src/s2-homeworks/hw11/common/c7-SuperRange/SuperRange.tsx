93b69ec07847bd63c0af50eec30cbf74de4e4394
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-07 13:17:45 +0500

    style: add styles in sx

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,7 +4,18 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                color: '#00CC22',
+                width: '147px',
+                height: 4,
+                '& .MuiSlider-thumb': {
+                    height: 18,
+                    width: 18,
+                    backgroundColor: '#fff',
+                    border: '2px solid currentColor',
+                    '&:hover, &.Mui-focusVisible, &.Mui-active': {
+                        boxShadow: '0 0 0 6px rgba(0, 204, 34, 0.16)',
+                    },
+                },
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
