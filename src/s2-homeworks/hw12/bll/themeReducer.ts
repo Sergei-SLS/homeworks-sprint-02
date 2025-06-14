61f744f058f68097ab6a674169b367b969c19cf1
Sergei-SLS <lyamin.sls@gmail.com>
2025-06-14 17:37:56 +0500

    refactor: add type in themeReducer.ts

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,6 +1,17 @@
-const initState = {
+const initState: themeStateType = {
     themeId: 1,
 }
+
+type themeStateType = {
+    themeId: number,
+}
+
+type changeThemeIdActionType = {
+    type: 'SET_THEME_ID',
+    id: number,
+}
+
+type actionType = changeThemeIdActionType
 
 export const themeReducer = (state = initState, action: any): any => { // fix any
     switch (action.type) {
